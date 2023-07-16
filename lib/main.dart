d8b0d619f2ba2fdfaf65fef02237f50a5d23b9ba
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-07-16 18:28:31 +0300

    initialize repo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather App',
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
