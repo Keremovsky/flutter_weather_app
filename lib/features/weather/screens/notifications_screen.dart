51b8a96cc4f2979c12f2bd7aac66ca158927c0b3
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-08-06 18:29:24 +0300

    get city name for notification

--- lib/features/weather/screens/notifications_screen.dart
+++ lib/features/weather/screens/notifications_screen.dart
@@ -1,28 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_weather_app/core/state_notifiers/saved_cities_notifier.dart';
 import 'package:flutter_weather_app/features/notification/controller/notification_controller.dart';
 
-class NotificationsScreen extends ConsumerWidget {
+class NotificationsScreen extends ConsumerStatefulWidget {
   static final routeName = "/notificationsScreen";
 
   const NotificationsScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends ConsumerState<NotificationsScreen> {
+  late FixedExtentScrollController controller;
+  List<String> notCities = ["Current City"];
+  late List<String> savedCities;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FixedExtentScrollController();
+
+    savedCities = ref.read(savedCitiesNotifierProvider);
+    notCities.addAll(savedCities);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          const Divider(),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Select City",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 300,
+            child: ListWheelScrollView.useDelegate(
+              controller: controller,
+              itemExtent: 100,
+              physics: const FixedExtentScrollPhysics(),
+              diameterRatio: 0.9,
+              childDelegate: ListWheelChildBuilderDelegate(
+                childCount: notCities.length,
+                builder: (context, index) {
+                  return Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(notCities[index]),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
                 onPressed: () {
+                  final selectedCity = notCities[controller.selectedItem];
                   ref
                       .read(notificationControllerProvider.notifier)
                       .setScheduleNotification(
                         context,
-                        "Kırklareli",
+                        selectedCity,
                         const Duration(seconds: 10),
                         10,
                       );
